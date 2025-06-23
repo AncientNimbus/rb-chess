87fef760054066d878b704fde58072588ab740bf
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-06-23 19:20:55 +0100

    test: the serialisation format in yaml and json
    refactor: Update user data save path to user_data

--- lib/user_profile.rb
+++ lib/user_profile.rb
@@ -6,15 +6,17 @@ require_relative "file_utils"
 module ConsoleGame
   # User Profile class
   class UserProfile
+    attr_reader :profile
     attr_accessor :username
 
     def initialize(username = "Arcade Player")
       @username = username
+      @profile = create_profile
     end
 
     # Create a user profile
     def create_profile
-      { uuid: SecureRandom.uuid, username: username, saved_date: Time.now.ceil, appdata: nil, stats: nil }
+      { uuid: SecureRandom.uuid, username: username, saved_date: Time.now.ceil, appdata: {}, stats: {} }
     end
   end
 end
