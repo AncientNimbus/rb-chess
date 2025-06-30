00b858fe15e3833174f44b7fbf42ac704aa84edc
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-06-30 11:20:39 +0100

    feat: Add the ability to change side decorator
    feat: Add more default values to BOARD design constants

--- lib/console_game/chess/display.rb
+++ lib/console_game/chess/display.rb
@@ -8,33 +8,45 @@ module ConsoleGame
     module Display
       # Default design for the chessboard
       BOARD = {
-        head: nil,
+        bg_theme: %w[#ada493 #847b6a],
+        file: [*"a".."h"],
+        std_tile: 3,
+        xl_tile: 7,
+        h: "═",
+        head_l: "╔═══╦", head_r: "╦═══╗",
+        sep_l: "╠═══╬", sep_r: "╬═══╣",
+        tail_l: "╚═══╩", tail_r: "╩═══╝",
         side: ->(v) { "║ #{v} ║" },
-        tail: nil,
-        bg_theme: %w[#ada493 #847b6a]
+        ends: ->(left, right, length, v) { "#{left}#{v.center(length * 8, v)}#{right}" }
       }.freeze
 
       # Print the chessboard
-      def print_board
+      # @param colors [Array<Symbol, String>] Expects contrasting background colour
+      # @param tile_w [Integer] width of each tile
+      # @param show_r [Boolean] print ranks on the side?
+      # @return [Array<String>] a complete board with head and tail
+      def build_board(turn_data, colors: BOARD[:bg_theme], tile_w: BOARD[:std_tile], show_r: false)
       end
 
       # Row formatter
       # @param row_num [Integer] row number
-      # @param items [Array<Hash>] expects a 1-element(reprinting) or 8-elements array(In order)
-      # @option items [String] :asset element in the tile
-      # @option items [Symbol] :color colour of the element
+      # @param row_data [Array<Hash>] expects a 1-element(reprinting) or 8-elements array(In order)
+      # @option row_data [String] :asset element in the tile
+      # @option row_data [Symbol] :color colour of the element
       # @param colors [Array<Symbol, String>] Expects contrasting background colour
       # @param tile_w [Integer] width of each tile
       # @param show_r [Boolean] print ranks on the side?
+      # @param label [String] override the print rank value with custom string
       # @return [Array<String>] a complete row in a board
-      def format_row(row_num, items, colors: BOARD[:bg_theme], tile_w: 3, show_r: false)
+      def format_row(row_num, row_data, colors: BOARD[:bg_theme], tile_w: BOARD[:std_tile], show_r: false, label: "")
         arr = []
         # Light background colour, dark background colour
         bg1, bg2 = pattern_order(row_num, colors: colors)
         # Build individual tile
-        8.times { |i| arr << paint_tile(items[i % items.size], tile_w, i.even? ? bg1 : bg2) }
+        8.times { |i| arr << paint_tile(row_data[i % row_data.size], tile_w, i.even? ? bg1 : bg2) }
         # Build side borders
-        side = [BOARD[:side].call(show_r ? row_num : " ")]
+        label = row_num if label.empty?
+        side = [BOARD[:side].call(show_r ? label : " ")]
         [side.concat(arr, side).join("")]
       end
 
