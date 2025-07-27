eb5a400517cd8b330159325f220a3c7d959f4b1d
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-07-27 11:40:11 +0100

    test: New test cases for error handling on load

--- spec/console_game/chess/game_spec.rb
+++ spec/console_game/chess/game_spec.rb
@@ -75,6 +75,38 @@ describe ConsoleGame::Chess::Game do
       end
     end
 
+    context "when user wishes to load an ongoing game session where the game mode is invalid" do
+      let(:test_sessions) { { "1" => { event: "Chess Sessions Integration test", site: "Chess game menu", date: Time.new(2025, 7, 26), round: nil, white: "Ancient", black: "Computer", result: nil, mode: 3, moves: {}, fens: ["rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"] } } }
+
+      before do
+        allow($stdout).to receive(:puts)
+        chess_manager.instance_variable_set(:@sessions, test_sessions)
+        allow(game_manager).to receive(:save_user_profile)
+        allow(game_manager).to receive(:exit_arcade).and_raise(SystemExit)
+      end
+
+      it "exit load saves mode, enters new game mode and exit successfully" do
+        allow(Readline).to receive(:readline).and_return("", "", "", "2", "1", "--exit")
+        expect { chess_manager.start }.to raise_error(SystemExit)
+      end
+    end
+
+    context "when user wishes to load an ongoing game session where computer opponent's name has been corrupted" do
+      let(:test_sessions) { { "1" => { event: "Chess Sessions Integration test", site: "Chess game menu", date: Time.new(2025, 7, 26), round: nil, white: "Ancient", black: "Bad Computer", result: nil, mode: 2, moves: {}, fens: ["rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"] } } }
+
+      before do
+        allow($stdout).to receive(:puts)
+        chess_manager.instance_variable_set(:@sessions, test_sessions)
+        allow(game_manager).to receive(:save_user_profile)
+        allow(game_manager).to receive(:exit_arcade).and_raise(SystemExit)
+      end
+
+      it "exit load saves mode, enters new game mode and exit successfully" do
+        allow(Readline).to receive(:readline).and_return("", "", "", "2", "1", "--exit")
+        expect { chess_manager.start }.to raise_error(SystemExit)
+      end
+    end
+
     context "when user loads a valid ended game session" do
       let(:test_sessions) { { "1" => { event: "Chess Sessions Integration test", site: "Chess game menu", date: Time.new(2025, 7, 26), round: nil, white: "Ancient", black: "Nimbus", result: nil, mode: 1, moves: {}, fens: ["rnbqkbnr/ppppp2p/5p2/6pQ/8/4P3/PPPP1PPP/RNB1KBNR b KQkq - 1 1"] } } }
 
